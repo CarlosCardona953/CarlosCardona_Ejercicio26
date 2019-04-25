97f74a40ea70b39fc37971ad6531276978d4755c
CarlosDCN <48950589+CarlosCardona953@users.noreply.github.com>
2019-04-25 07:49:09 -0500

    Add files via upload

--- CarlosCardona_Ejercicio26.cpp
+++ CarlosCardona_Ejercicio26.cpp
@@ -6,15 +6,15 @@ using namespace std;
 
 void solve_equation_rgk(float x_init, float x_end, float delta_x, float omega, string filename);
 void solve_equation_euler(float x_init, float x_end, float delta_x, float omega, string filename);
-//void solve_equation_leapfrog(float x_init, float x_end, float delta_x, float omega, string filename);
+void solve_equation_leapfrog(float x_init, float x_end, float delta_x, float omega, string filename);
 
 int main(){
    
   //Para este caso tomamos el x inicial como 0 y el final como 30  
   float omega=1.0;
-  solve_equation_rgk(0.0, 10000.0, omega/2, omega, "rgk_01.dat");
+  solve_equation_rgk(0.0, 10000.0, omega/2, omega, "rgk.dat");
   solve_equation_euler(0.0, 10000.0, omega/2, omega, "euler.dat");
-  //solve_equation_leapfrog(0.0, 10000.0, omega/2, omega, "leapfrog.dat");
+  solve_equation_leapfrog(0.0, 10000.0, omega/2, omega, "leapfrog.dat");
  
   return 0;
 }
@@ -42,17 +42,25 @@ void solve_equation_rgk(float x_init, float x_end, float delta_x, float omega, s
     z_old=z;    
       
     k0= y_old*f;
-    k1= (y_old + 0.5*k0*delta_x)*(f); 
-    k2= (y_old + 0.5*k1*delta_x)*(f); 
-    k3= (y_old + k2*delta_x)*(f); 
+    m0= z_old ;
+    z = z_old + delta_x*k0*0.5;    
+    y = y_old + delta_x*m0*0.5;
+          
+    k1= y_old*f; 
+    m1= z_old; 
+    z = z_old + delta_x*k1*0.5;    
+    y = y + delta_x*m1*0.5;
+          
+    k2= y_old*f;
+    m2= z_old; 
+    z = z_old + delta_x*k2;    
+    y = y_old + delta_x*m2;
+          
+    k3= y_old*f; 
+    m3= z_old; 
     
-    kp= (k0+(k1*2)+(k2*2)+k3)/6;
-    
-    m0= z_old + f*delta_x*y_old;
-    m1= z_old +((y_old + 0.5*k0*delta_x)*(delta_x)*f); 
-    m2= z_old +((y_old + 0.5*k1*delta_x)*(delta_x)*f); 
-    m3= z_old +((y_old + k2*delta_x)*(delta_x)*f); 
-    
+          
+    kp= (k0+(k1*2)+(k2*2)+k3)/6;    
     mp= (m0+(m1*2)+(m2*2)+m3)/6;
     
     z = z_old + delta_x*kp;    
@@ -95,4 +103,35 @@ void solve_equation_euler(float x_init, float x_end, float delta_x, float omega,
   
   }
   outfile.close();
+}
+
+
+void solve_equation_leapfrog(float x_init, float x_end, float delta_x, float omega, string filename){
+    
+  float x=x_init;
+  float y=1.0;
+  float z=0;
+    
+  float f = -omega*omega;
+    
+  ofstream outfile;
+  outfile.open(filename);
+  
+    while(x<x_end){
+      
+    outfile << x << " " << y <<" " << z << endl;
+  
+        
+    z = z + delta_x*y*f*0.5;    
+    y = y + delta_x*z;
+    z = z + y*f*delta_x*0.5;
+       
+    x = x + delta_x;
+    
+        
+  
+  }
+  outfile.close();
+ 
+    
 }
